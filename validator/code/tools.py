ccf386670c83d9e51e8a74b5bcab6b08c3d191c3
Minaev Oleg <O.Minaev@skoltech.ru>
2019-12-13 12:52:36 +0300

    Объект компании с рекурсивным доступом к собственным атрибутам

--- validator/code/tools.py
+++ validator/code/tools.py
@@ -2,3 +2,25 @@
     Модуль с полезными утилитами
 """
 
+
+class Company:
+
+    def __init__(self, data):
+        self.values = data
+        print(f"[ Company ] Values: {self.values}")
+
+    def __getitem__(self, key):
+        if key == "":
+            return None
+        elif "." not in key:
+            return self.values[key]
+        else:
+            items_list = key.split('.')
+            return self.recursive_value(self.values, items_list)
+
+    def recursive_value(self, dct, items_list):
+        if len(items_list) > 1:
+            item = items_list.pop(0)
+            return self.recursive_value(dct[item], items_list)
+        else:
+            return dct[items_list[0]]
