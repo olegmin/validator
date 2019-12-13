19c99485d9c7d4aaf1e2c6754c43cd2b4fc0425c
Minaev Oleg <O.Minaev@skoltech.ru>
2019-12-13 11:31:03 +0300

    Готов обработчик файлов JSON

--- validator/code/run.py
+++ validator/code/run.py
@@ -1,5 +1,12 @@
 """
     Точка входа в приложение
 """
+import os
+from filehadler import json_reader
 
-print("Hello, world!")
+
+if __name__ == "__main__":
+    dir_path = "/tmp/conditions/"
+    for file in os.listdir(path=dir_path):
+        data = json_reader(dir_path+file)
+        print(f"\n{data}\n\ttype:{type(data)}\n")
